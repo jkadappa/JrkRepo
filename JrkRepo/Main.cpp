4b743798cc51683bd1c35d9e1d23bec999936a73
kadappan jothi <jkadappa@syr.edu>
2013-05-15 16:42:04 -0400

    Intial Draft Commit by JRK

---
+++ JrkRepo/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+void add(int a,int b)
+{
+	std::cout<<"the sum of two numbers"<<a+b<<std::endl; 	
+}
+void sub(int a,int b)
+{
+	std::cout<<"the sum of two numbers"<<a-b<<std::endl; 
+}
+void mul(int a,int b)
+{
+	std::cout<<"the sum of two numbers"<<a*b<<std::endl; 
+}
+
+
+
+int main(int argc, char * argv[])
+{
+add(2,2);
+sub(2,2);
+}
